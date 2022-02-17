39643e2e4cb9094cb393261d80091c8c215d2bae
Maxim-prog-cyber <maks.kuryzin@yandex.ru>
2022-02-17 16:06:47 +0300

    yrtftry

--- src/GGG.java
+++ src/GGG.java
@@ -1,5 +1,8 @@
 public class GGG {
     public static void main(String[] args) {
         System.out.println("fedf");
+        System.out.println("fedf");
+        System.out.println("fedf");
+        System.out.println("fedf");
     }
 }
